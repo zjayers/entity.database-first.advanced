f96f2f7487ce1a35ea3758131eacf16ab1051652
Zach Ayers <ayers.dev@icloud.com>
2020-10-15 12:10:22 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.DatabaseFirst.Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoDbContext();
+            var courses = context.GetCourses();
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Title);   
+            }
+        }
+    }
+}
